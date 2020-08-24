1af039196f2653b1ffa38f6ac87ff287537682d9
Stathis Georgiou <eust.georgiou@gmail.com>
2020-08-24 15:11:13 +0300

    plugin updates v1

---
+++ Plugins/ARPolis/ARPolis/Data/JsonGenericData.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ARPolis.Data
+{
+    /// <summary>
+    /// Responsible for assigning JSON Data to Class Structure
+    /// </summary>
+    public class JsonGenericData : MonoBehaviour
+    {
+
+        /// <summary>
+        /// Loads list of objects reading a json file
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="jsonFilePath">must be into Resources folder</param>
+        /// <returns></returns>
+        public static T[] LoadDataFromJson<T>(string jsonFilePath)
+        {
+            string jsonData = FixJsonItems(LoadResourceTextfile(jsonFilePath));
+
+            if (string.IsNullOrEmpty(jsonData))
+            {
+                Debug.Log("error reading " + jsonFilePath);
+                return null;
+            }
+            else
+            {
+                //if json has limited data return null
+                //something is wrong or the file is corrupted
+                if (jsonData.Length < 10)
+                {
+                    Debug.Log("error on file " + jsonFilePath);
+                    return null;
+                }
+                else
+                {
+                    return JsonHelper.FromJson<T>(jsonData);
+                }
+            }
+        }
+                
+        public static IEnumerator LoadJsonData<T>(string filename)//, List<T> t)
+        {
+            //ios
+            if (CommonData.ISiOS())
+            {
+                FileInfo fInfo = new FileInfo(filename);
+                if (fInfo == null || !fInfo.Exists)
+                {
+                    Debug.Log("NOT FOUND: " + filename);
+                    yield return null;
+                }
+                else
+                {
+                    string myjsondata = File.ReadAllText(fInfo.FullName);
+                    if (myjsondata.Length > 10)
+                    {
+                        myjsondata = FixJsonItems(myjsondata);
+                        yield return JsonHelper.FromJson<T>(myjsondata).ToList();
+                    }
+                    else
+                    {
+                        Debug.Log("empty - NOT FOUND: " + filename);
+                        yield return null;
+                    }
+                }
+            }
+            else
+            {
+                UnityWebRequest www = UnityWebRequest.Get(filename);
+                yield return www.SendWebRequest();
+
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Debug.Log(www.error);
+                    yield return null;
+                }
+                else
+                {
+                    string jsonData = www.downloadHandler.text;
+                    if (string.IsNullOrEmpty(jsonData) || jsonData.Length < 10)
+                    {
+                        yield return null;
+                    }
+                    else
+                    {
+                        jsonData = FixJsonItems(www.downloadHandler.text);
+                        yield return JsonHelper.FromJson<T>(jsonData).ToList();
+                    }
+                }
+            }
+        }
+
+
+        private static string LoadResourceTextfile(string filePath)
+        {
+            TextAsset targetFile = Resources.Load<TextAsset>(filePath);
+
+            if (targetFile == null)
+            {
+                Debug.LogWarning(filePath + ".json is missing!!!");
+                return string.Empty;
+            }
+            return targetFile.text;
+        }
+
+        private static string FixJsonItems(string value)
+        {
+            value = "{\"Items\":" + value + "}";
+            return value;
+        }
+
+    }
+
+}
---
+++ Plugins/ARPolis/Examples/ExampleCases.cs
@@ -0,0 +1,76 @@
+﻿using ARPolis.Data;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ARPolis.Examples
+{
+
+    public class ExampleCases : MonoBehaviour
+    {
+        AudioClip myAudioClip;
+        RawImage image;
+        List<JsonClassTest> jsonClassTests;
+
+        //Json
+        private IEnumerator JsonToClass()
+        {
+            string jsonTestFile = Path.Combine(Application.streamingAssetsPath, "jsonTest");
+            jsonTestFile += ".json";
+
+            CoroutineWithData cd = new CoroutineWithData(this, StaticData.LoadJsonData<JsonClassTest>(jsonTestFile));
+            yield return cd.Coroutine;
+            if (cd.result == null) { Debug.LogWarning("Error reading " + jsonTestFile); }
+            else
+            {
+                jsonClassTests = cd.result as List<JsonClassTest>;
+            }
+        }
+
+        //Image
+        private IEnumerator GetImageFileExample()
+        {
+            string imageFile = Path.Combine(Application.streamingAssetsPath, "image.png");//file must be into StreamingAssets folder
+            Debug.Log(imageFile);
+
+            CoroutineWithData cd = new CoroutineWithData(this, DigitalGenericData.LoadTextureData<Texture2D>(imageFile));
+            yield return cd.Coroutine;
+            if (cd.result == null)
+            {
+                Debug.LogWarning("Error reading " + imageFile);
+            }
+            else
+            {
+                image.texture = (Texture2D)cd.result;
+            }
+        }
+        
+        //Audio
+        private IEnumerator GetAudioFileExample()
+        {
+            string imageFile = Path.Combine(Application.streamingAssetsPath, "audio.ogg");//file must be into StreamingAssets folder
+            Debug.Log(imageFile);
+
+            CoroutineWithData cd = new CoroutineWithData(this, DigitalGenericData.LoadAudioData<AudioClip>(imageFile));
+            yield return cd.Coroutine;
+            if (cd.result == null)
+            {
+                Debug.LogWarning("Error reading " + imageFile);
+            }
+            else
+            {
+                myAudioClip = (AudioClip)cd.result;
+            }
+        }
+    }
+
+    [Serializable]
+    public class JsonClassTest
+    {
+        public string id, topic_ID, attribute_ID, testimony_ID, attribute_Lang, attribute_Label, attribute_Value;
+    }
+
+}
